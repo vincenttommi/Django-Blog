9e7ae701ad35cecaa833536db56362982210ecf4
vincenttommi <vincenttommi@gmail.com>
2023-11-09 12:27:30 +0300

    setting up projects

--- mysite/blog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Post
-
-admin.site.register(Post)
-class  PostAdmin(admin.ModelAdmin):
-    list_display = ['title','slug','author','publish','status']
-    
