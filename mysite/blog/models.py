c291d84e7176dd1ef39adf44aa89815bc9dc77bb
vincenttommi <vincenttommi@gmail.com>
2023-11-14 11:42:41 +0300

    updating list html

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db.models.query import QuerySet
 from django.utils import timezone
 from django.contrib.auth.models import User
-#importing user
+from  django.urls import reverse
 # Create your models here.
 
 
@@ -56,3 +56,10 @@ class  Post(models.Model):
         #databse index  - improves performance for  queries filtering or ordering results
     def  __str__(self):
         return self.title
+    
+    
+    
+    
+def  get_absolute_url(self):
+    return reverse('blog:post_detail',args=[self.id])
+    
