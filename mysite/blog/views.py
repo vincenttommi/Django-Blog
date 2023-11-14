e4262358ed02ee33d633f408120b33f49ae4a73c
vincenttommi <vincenttommi@gmail.com>
2023-11-14 15:12:02 +0300

    registering class based views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,7 +3,7 @@ from .models import Post
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage,\
  PageNotAnInteger
-
+from  django.views.generic import ListView
 # In this view, we retrieve all the posts with 
 # the PUBLISHED status using the published manager that we created previously
 def post_list(request):
@@ -32,3 +32,17 @@ def post_detail(request, year, month, day, post):
     post = get_list_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day)
     # In the detail view, we now use the get_object_or_404() shortcut to retrieve the desired post
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+
+class  PostListView(ListView):
+    """
+    
+    Alternative post list  view
+    """
+    queryset  = Post.published.all()
+    content_object_name  =  'posts'
+    paginate_by = 3
+    template_name  = '/post/list.html'
+    
+# we have implemented a class-based view that inherits from the ListView class    
