87e0205100a0dbac6f769648126eed07b96c832c
vincenttommi <vincenttommi@gmail.com>
2023-11-14 14:48:39 +0300

    managing pagination errors

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,35 +1,29 @@
-from django.shortcuts import render,get_list_or_404
+from django.shortcuts import render, get_list_or_404
 from .models import Post
 from django.http import Http404
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage
 
-
-#In this view, we retrieve all the posts with 
+# In this view, we retrieve all the posts with 
 # the PUBLISHED status using the published manager that we created previously
-
-def  post_list(request):
-    post_list   =  Post.published.all()
+def post_list(request):
+    post_list = Post.published.all()
     
-    #pagination with 3 posts per range
-    paginator  = Paginator(post_list, 3)
-    page_number  = request.GET.get('page', 1)
-    posts  = paginator.page(page_number)
-    
-    return render(request,'post/list.html',{'posts':posts})
+    # Pagination with 3 posts per range
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+ 
+    # We have added a try and except block to manage 
+    # the EmptyPage exception when retrieving a page
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        # If page_number is out of range, deliver the last page of results 
+        posts = paginator.page(paginator.num_pages)
 
+    return render(request, 'post/list.html', {'posts': posts})
 
-#create a view to  return a single post
-
+# Create a view to return a single post
 def post_detail(request, year, month, day, post):
-    
-        post =  get_list_or_404(Post,status=Post.Status.PUBLISHED,slug=post,publish__year=year,publish__month=month,publish__day=day)
-       #In the detail view, we now use the get_object_or_404() shortcut to retrieve the desired post
-        return render(request, 'blog/post/detail.html',{'post':post})  
-    
-    
-    
-
-    
-      
-        
-        
+    post = get_list_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day)
+    # In the detail view, we now use the get_object_or_404() shortcut to retrieve the desired post
+    return render(request, 'blog/post/detail.html', {'post': post})
