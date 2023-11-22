db2a2c83e41d5a6770e785cf59339f0968aa5632
vincenttommi <vincenttommi@gmail.com>
2023-11-22 10:56:08 +0300

    creating a comment section

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -9,8 +9,9 @@ from  django.urls import reverse
 
 
 class PublishedManager(models.Manager):
-    def get_queryset(self):
-        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+  def get_queryset(self):
+      return super().get_queryset()\
+                   .filter(status=Post.Status.PUBLISHED)
 #This manager retrieves all the  objects from database
 
 
@@ -21,31 +22,17 @@ class PublishedManager(models.Manager):
 class  Post(models.Model):
     
     class Status(models.TextChoices):
-        #adding status field   to our model that will allow us to manage  status of blog posts
         DRAFT = 'DF','Draft'
         PUBLISHED  = 'PB','Published'
       
     title  = models.CharField(max_length=250)
     slug  = models.SlugField(max_length=250, unique_for_date='publish')
-#By using unique_for_date, the slug field is now required to be unique for the date stored in the
-# publish field.
-    author  = models.ForeignKey(User, on_delete=models.CASCADE,related_name='blog_posts')
-    #We have imported the User model from the django.contrib.auth.models module and we have added
-  #an author field to the Post model. This field defines a many-to-one relationship, meaning that each
-  #post is written by a user, and a user can write any number of posts.
-    
+    author  = models.ForeignKey(User, on_delete=models.CASCADE,related_name='blog_posts')  
     body  =  models.TextField()
     publish  =  models.DateTimeField(default=timezone.now)
-    #publish: This is a DateTimeField field that translates 
-    # into a DATETIME column in the SQL database
     created  =  models.DateTimeField(auto_now_add=True)
-    #created: This is a DateTimeField field. We will use it to store the date and time when the post
-  #was created. By using auto_now_add, the date will be saved automatically when creating anobject.
     updated =  models.DateTimeField(auto_now=True)
-#updated: This is a DateTimeField field. We will use it to store the last date and time when the
- #post was updated. By using auto_now, the date will be updated automatically when saving an object.
-     
-#Defining  a defualt sort order that arrranges post in a chronolgical order
+
     status  = models.CharField(max_length=2,choices=Status.choices, default=Status.DRAFT)
     objects  = models.Manager()  #The default manager
     published =  PublishedManager() #my custom manager
@@ -56,7 +43,6 @@ class  Post(models.Model):
         indexes = [
             models.Index(fields=['-publish'])
         ]
-        #databse index  - improves performance for  queries filtering or ordering results
     def  __str__(self):
         return self.title
     
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage,\
@@ -7,6 +7,7 @@ from django.core.paginator import Paginator, EmptyPage,\
 from .forms  import  EmailPostForm
 #importing class of the form to our function
 from django.core.mail  import  send_mail
+from django.views.generic import ListView
 #importing mail from django.core.mail
 
 
@@ -22,6 +23,7 @@ from django.core.mail  import  send_mail
 def post_list(request):
     post_list = Post.published.all()
     
+    print('post', post_list)
     # Pagination with 3 posts per range
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
@@ -38,67 +40,66 @@ def post_list(request):
         # if page_number is out of range deliver  last page  of results
         posts = paginator.page(paginator.num_pages)
 
-    return render(request, 'post/list.html', {'posts': posts})
+    return render(request, 'blog/post/list.html', {'posts': posts})
 
 # Create a view to return a single post
 def post_detail(request, year, month, day, post):
-    post = get_list_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day)
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day)
     # In the detail view, we now use the get_object_or_404() shortcut to retrieve the desired post
-    return render(request, 'post/detail.html', {'post': post})
+    return render(request, 'blog/post/detail.html', {'post': post})
 
 
 
-# class  PostListView(ListView):
-#     """
+class  PostListView(ListView):
+    """
     
-#     Alternative post list  view
-#     """
-#     queryset  = Post.published.all()
-#     content_object_name  =  'posts'
-#     paginate_by = 3
-#     template_name  = '/post/list.html'
+    Alternative post list  view
+    """
+    queryset  = Post.published.all()
+    content_object_name  =  'posts'
+    paginate_by = 3
+    template_name  = 'blog/post/list.html'
     
 # we have implemented a class-based view that inherits from the ListView class    
 
 
 
-def  post_share(request, post_id):
-    #Retrieving  post by id
+
+
+def post_share(request, post_id):
+    # Retrieving post by id
     
-    post  = get_list_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
-# We have defined the post_share view that takes the request object and the post_id variable as pa-
- #  rameters. We use the get_object_or_404() shortcut to retrieve a published post by its id.
+    post  = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+
+    # We have defined the post_share view that takes the request object and the post_id variable as parameters.
+    # We use the get_object_or_404() shortcut to retrieve a published post by its id.
+    sent = False
+    
     if request.method == 'POST':
-        #Form  was submitted 
+        # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
-            #Form fields passed  validation
+            # Form fields passed validation
             cd = form.cleaned_data
-            
-            #send email
-            post_url  = request.build_absolute_uri(post.get_absolute_url())
-            #this code generates an absolute URL for a specific Post within context of webrequest
-            
-            subject  = f"{cd['name']} recommends you read" \
-                f"{post.title}"
-            
-            message =  f"Read{post.title} at {post_url}\n\n"\
-                f"{cd['name']}\'s comments:{cd['comments']}"
-             
-            send_mail(subject, message, 'vincenttommi@gmail.com',
-            [cd['to']])        
-                
-                
-            sent  = True
-            
-            
+
+            # Send email
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            # This code generates an absolute URL for a specific Post within the context of the web request
+
+            subject = f"{cd['name']} recommends you read {post.title}"
+
+            message = f"Read {post.title} at {post_url}\n\n{cd['name']}'s comments: {cd['comments']}"
+
+            send_mail(subject, message, 'vincenttommi@gmail.com', [cd['to']])
+
+            sent = True
+
     else:
         form = EmailPostForm()
-        
-        #when  page is loaded for the first time,the view is receives a GET request
-        #in this case a  new EmailPostForm is created and stored in form variable
-        return render(request, 'post/share.html',{'post':post,'form':'form', 'sent':sent})
-    
-    
 
-    
+    # When the page is loaded for the first time, the view receives a GET request.
+    # In this case, a new EmailPostForm is created and stored in the form variable.
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+
+
